31e03ea65482d1abb931390f56f60180a3d22b25
Lenald <lenaldnwj@gmail.com>
2020-03-09 17:12:38 +0800

    Edit about page text

--- src/javascript/components/settings/about.js
+++ src/javascript/components/settings/about.js
@@ -6,7 +6,6 @@ function returnAbout(){
             <gv-blocktitle>${getTranslation('About')} </gv-blocktitle>
             <gv-blockcontent>
                 <p>${getTranslation('About-text1')}</p>
-                <p>${getTranslation('About-text2')}</p>
                 <button class="button1" onclick="shell.openExternal('https://www.graviton.ml')">Website</button>
                 <button class="button1" onclick="shell.openExternal('https://github.com/Graviton-Code-Editor/Graviton-App/')">Source Code</button>
                 <button class="button1" onclick="shell.openExternal('https://github.com/Graviton-Code-Editor/Graviton-App/blob/master/LICENSE.md')">License</button>
