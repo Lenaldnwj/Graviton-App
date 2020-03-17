4577882e3b109db7e94d295dd4b60ad60c365671
Lenald <lenaldnwj@gmail.com>
2020-03-17 19:57:51 +0800

    Control component refactor

--- src/javascript/api/constructors/control.js
+++ src/javascript/api/constructors/control.js
@@ -16,33 +16,34 @@ new Control({
   onClick: ()=> console.log("clicked me!")
 })
 */
+function Control ({ text, hint, screen = current_screen.id, onClick }) {
+  this.text = text
+  this.hint = hint
+  this.screen = screen
+  this.onClick = onClick
+  const controlSpan = document.createElement('span')
+  controlSpan.innerText = text
+  if (hint != undefined) controlSpan.title = hint
+  document
+    .getElementById(this.screen)
+    .children[2].appendChild(controlSpan)
+  this.setText = newText => {
+    controlSpan.innerText = newText
+  }
+  if(onClick){
+    controlSpan.onclick = onClick
+  }
+  this.setHint = newHint => {
+    controlSpan.title = newHint
+  }
+  this.hide = () => {
+    controlSpan.style.display = 'none'
+  }
+  this.show = () => {
+    controlSpan.style.display = 'block'
+  }
+}
 
 module.exports = {
-  Control: function ({ text, hint, screen = current_screen.id, onClick }) {
-    this.text = text
-    this.hint = hint
-    this.screen = screen
-    this.onClick = onClick
-    const controlSpan = document.createElement('span')
-    controlSpan.innerText = text
-    if (hint != undefined) controlSpan.title = hint
-    document
-      .getElementById(this.screen)
-      .children[2].appendChild(controlSpan)
-    this.setText = newText => {
-      controlSpan.innerText = newText
-    }
-    if(onClick){
-      controlSpan.onclick = onClick
-    }
-    this.setHint = newHint => {
-      controlSpan.title = newHint
-    }
-    this.hide = () => {
-      controlSpan.style.display = 'none'
-    }
-    this.show = () => {
-      controlSpan.style.display = 'block'
-    }
-  }
+  Control: Control
 }
