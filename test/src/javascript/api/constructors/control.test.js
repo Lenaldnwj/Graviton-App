fb8789ae5a5d7e14776147491717d6ae1946f7cb
Lenald <lenaldnwj@gmail.com>
2020-04-15 13:34:54 +0800

    Refactor control

--- test/src/javascript/api/constructors/control.test.js
+++ test/src/javascript/api/constructors/control.test.js
@@ -8,13 +8,4 @@ describe('Test Control Component', () => {
     expect(Control).toBeDefined()
   })
 
-  // test('Control function input', () => {
-  //   var htmlElement
-  //   htmlElement = document.createElement('div')
-  //   htmlElement.setAttribute('id', '67')
-  //
-  //   Control({ text: 'textholder', hint: 'hintholder', screen: '67', onClick: 1})
-  //   expect(document.getElementById('67')).toBe('textholder')
-  // })
-
 })
